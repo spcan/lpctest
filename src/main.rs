618f5043f0643c758fa048b86afc05fdab63c1f2
agrc <agrc14@gmail.com>
2024-01-11 01:04:53 +0100

    misc: Scratch changes

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@
 use defmt_rtt as _;
 //use lpc5500 as _;
 use cortex_m as _;
+use lpc5500::system::user::UserSystemControl;
 
 
 
@@ -20,19 +21,9 @@ fn main(hal: lpc5500::Peripherals) -> ! {
 
     defmt::error!("Hello from main");
 
-    // Get the user interface.
+    // Get the user interface and configure the device.
     let mut user = hal.user;
-
-    // Get the clock state.
-    let (frequency, source) = user.main.getclock();
-    defmt::debug!("Device is using {} as main clock [{} Hz]", source, frequency);
-
-    user.setclock(lpc5500::system::user::ClockSource::FRO1Mhz);
-    let (frequency, source) = user.main.getclock();
-    defmt::debug!("Device is using {} as main clock [{} Hz]", source, frequency);
-
-    // Configure Flash acceleration.
-    user.flash.acceleration( Some( accelcfg() ) );
+    sysconfig(&mut user);
 
     // Get the Port 1 pins.
     let pins = hal.pins.PORT1;
@@ -56,9 +47,9 @@ fn main(hal: lpc5500::Peripherals) -> ! {
 
     // Configure this set of frequencies and repeats.
     let sequence = [
-        (lpc5500::system::user::ClockSource::FRO96Mhz, 100000, 5),
-        (lpc5500::system::user::ClockSource::FRO12Mhz, 100000, 5),
-        (lpc5500::system::user::ClockSource::FRO1Mhz , 100000, 5),
+        (lpc5500::system::user::ClockSource::FRO96Mhz, 500000, 60),
+        (lpc5500::system::user::ClockSource::FRO12Mhz, 100000,  5),
+        (lpc5500::system::user::ClockSource::FRO1Mhz , 100000,  3),
     ];
 
     loop {
@@ -96,6 +87,8 @@ fn coprocessor(pq: lpc5500::powerquad::PowerQuad) {
     // Initialize the PowerQuad.
     let (mut cp0, mut cp1, mut engine) = pq.init();
 
+    defmt::info!("Registered PQ handler = 0x{:08X}", unsafe { core::ptr::read_volatile((0x20000000 + (4 * (16 + 57))) as *const u32) });
+
     // Select the divisors.
     let div0 = 2.0;
     let div1 = 3.0;
@@ -110,6 +103,16 @@ fn coprocessor(pq: lpc5500::powerquad::PowerQuad) {
 
     defmt::info!("exp({}) = {}", div0, res0);
     defmt::info!("exp({}) = {}", div1, res1);
+
+    // Execute divisions.
+    let op0 = cp0.div(2.0, 1.0);
+    let op1 = cp1.div(1.0, 2.0);
+
+    // Finish both instructions.
+    let res0 = op0.finish();
+    let res1 = op1.finish();
+
+    defmt::info!("2.0 / 1.0 = {}\n1.0 / 2.0 = {}", res0, res1);
 
     // Create a matrix.
     let mut eye: TypedMatrix<5, 5> = TypedMatrix::eye();
@@ -131,33 +134,47 @@ fn coprocessor(pq: lpc5500::powerquad::PowerQuad) {
 
     // Add A + B = C.
     defmt::info!("Launching matrix addition.");
-    //let _ = engine.add(&a, &b, &mut c);
+    let _ = engine.add(&a, &b, &mut c);
+
+    unsafe { core::arch::asm!("wfe", options(nomem, nostack)) };
 
 
     // Launch another operation to see if this conflicts.
     let r = cp0.exp(7.0);
 
-    defmt::info!("Waiting for amtrix addition to finish");
+    defmt::info!("Waiting for matrix addition to finish");
     let r7 = r.finish();
-    //engine.finish();
-    defmt::info!("Matrix addition is done.");
+    engine.finish();
+    //defmt::info!("Matrix addition is done.");
 
-    defmt::info!("exp(7) = {}", r7);
-    defmt::info!("A + B = {}", c);
+    //defmt::info!("exp(7) = {}", r7);
+    //defmt::info!("A + B = {}", c);
 }
 
 
-const fn accelcfg() -> lpc5500::system::user::Acceleration {
+
+#[inline(never)]
+fn sysconfig(user: &mut lpc5500::system::user::UserSystemControl) {
     use lpc5500::system::user::{
-        Acceleration, BufferUsage,
+        Acceleration, BufferUsage, ClockSource,
     };
 
-    Acceleration {
+    // FLASH acceleration configuration.
+    const ACCELERATION: Acceleration = Acceleration {
         dbuf: BufferUsage::All,
         ibuf: BufferUsage::All,
         prefetch: true,
-    }
+    };
+
+    // Set the clock state.
+    user.setclock(ClockSource::FRO96Mhz);
+    let (frequency, source) = user.main.getclock();
+    defmt::info!("Device is using {} as main clock [{} Hz]", source, frequency);
+
+    // Configure Flash acceleration.
+    user.flash.acceleration( Some( ACCELERATION ) );
 }
+
 
 
 
